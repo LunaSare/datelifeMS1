1b8bae9d36e87a7caf4e832a597f54f83e9f08e1
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-04-07 23:37:39 -0700

    cross validation XY figure

---
+++ R/cross-validation.R
@@ -0,0 +1,56 @@
+# Function based on https://github.com/LunaSare/datelife_examples/blob/master/R/functions_data.R#L3
+# get calibrations form all other chronograms not belonging to the same study
+congruify_other_calibrations <- function(phylo_all, index) {
+    # dd <- duplicated(names(phyloall))
+    cat(index, " -- ", names(phylo_all)[index], "\n")
+    study <- names(phylo_all) %in% names(phylo_all)[index]
+    res <- datelife::congruify_calibrations(phy = phylo_all[study][[1]],
+                                     chronograms = phylo_all[!study])
+    names(res) <- names(phylo_all)[index]
+    res
+}
+
+congruify_other_calibrations_all <- function(phylo_all){
+    # dd <- duplicated(names(phyloall))
+    res <- lapply(seq(phylo_all), function(i){
+        cat(i, " -- ", names(phylo_all)[i], "\n")
+        dd <- names(phylo_all) %in% names(phylo_all)[i]
+        datelife::congruify_calibrations(phy = phylo_all[dd][[1]],
+                                         chronograms = phylo_all[!dd])
+    })
+    names(res) <- names(phylo_all)
+    res
+}
+# Based on function use_othercals3 from https://github.com/LunaSare/datelife_examples/blob/master/R/functions_data.R#L36
+use_other_calibrations <- function(trees, other_calibrations, ...){
+    res <- lapply(seq(trees), function(i){
+      phy <- trees[[i]]
+      phy$edge.length <- NULL
+      print(i)
+        xx <- suppressMessages(suppressWarnings(datelife::use_calibrations_bladj(phy,
+            calibrations = other_calibrations[[i]], ...)))
+        return(xx)
+    })
+    class(res) <- "multiPhylo"
+    names(res) <- names(trees)
+    res
+}
+# Function to date source chronograms with data from other chronograms
+cross_date <- function(frin_other_calibs, index) {
+  cat(index, " -- ", names(frin_other_calibs)[index], "\n")
+  
+  calibrations <- frin_other_calibs[[index]]
+  
+  if (length(calibrations) == 1) {
+    message("... Source chronogram has no calibrations.")
+    return(NA)
+  }
+  
+  mm <- calibrations$matched_calibrations
+  mm <- mm[mm$congruent,]
+  mm <- mm[!is.na(mm$mrca_node_number),]
+  calibrations$matched_calibrations <- mm
+  res <- use_calibrations_bladj.matchedCalibrations(calibrations = calibrations, 
+                                                    type = "mean")
+  res
+}
