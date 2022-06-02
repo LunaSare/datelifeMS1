f7b5d3076dd5b2a861e3333e41eb7fd1f303ffd1
Luna Sare <sanchez.reyes.luna@gmail.com>
2022-06-01 23:32:37 -0700

    updating tables of all fringillidae example for supplements

--- R/make_pretty_tables.R
+++ R/make_pretty_tables.R
@@ -5,29 +5,23 @@
 # # Change node numbers by node names
 # colnames(some_table)
 
-make_pretty_table <- function(some_table, duplicated_criteria) {
-  # assign variables:
-  cols2keep_order <- c("mrca_node_name", "taxonA", "taxonB", "MaxAge", "reference")
+make_pretty_table <- function(some_table,
+                              duplicated_criteria,
+                              cols2keep_order,
+                              order_rows = FALSE,
+                              colnames_new){
+  # Eliminate duplicated rows:
   if (missing(duplicated_criteria)) {
     duplicated_criteria <- c("taxonA", "taxonB", "reference","MaxAge")
-  } else {
-    duplicated_criteria <- duplicated_criteria
   }
-  colnames_new <- c("Node Name", "taxon A", "taxon B", "Node Age", "Study reference")
-  # Eliminate duplicated rows:
   ## Find the duplicated rows within studies:
   ## Note: take into account age too, taxon pairs might come from different studies.
   taxon_pairs_ref <- c()
   for (i in seq(nrow(some_table))) {
-    print(i)
     tax_pair_ref <- as.character(some_table[i, duplicated_criteria])
     tax_pair_ref <- tax_pair_ref[order(tax_pair_ref)]
     tax_pair_ref <- paste(tax_pair_ref, collapse = "")
     taxon_pairs_ref <- c(taxon_pairs_ref, tax_pair_ref)
-  }
-  if (sum(duplicated(taxon_pairs_ref)) == 0) {
-    message("There are no duplicated data.")
-    pretty_calibs <- some_table
   }
   ## Eliminate duplicated rows if any:
   if (any(duplicated(taxon_pairs_ref))) {
@@ -35,19 +29,46 @@ make_pretty_table <- function(some_table, duplicated_criteria) {
     message("Eliminating duplicated data from rows: ", paste(indices, collapse = " | "))
     pretty_calibs <- some_table[!duplicated(taxon_pairs_ref),]
   }
-
+  # If no duplicated row, assign some_table variable to object pretty_table:
+  if (sum(duplicated(taxon_pairs_ref)) == 0) {
+    message("There are no duplicated calibrations.")
+    pretty_calibs <- some_table
+  }
+  if (order_rows) {
+    row_order <- order(pretty_calibs$mrca_node_number,
+                       pretty_calibs$MaxAge,
+                       pretty_calibs$reference,
+                       pretty_calibs$taxonA,
+                       pretty_calibs$taxonB)
+    pretty_calibs[row_order,]
+  }
   # Reorder ROWS by node number, reference, age and taxon names
-  row_order <- order(pretty_calibs$mrca_node_number, pretty_calibs$MaxAge, pretty_calibs$reference, pretty_calibs$taxonA, pretty_calibs$taxonB)
-  pretty_calibs <- pretty_calibs[row_order,]
+  if (missing(cols2order_rows)) {
+    cols2order_rows <- c("mrca_node_name",
+                         "MaxAge",
+                         "study",
+                         "taxonA",
+                         "taxonB")
+  }
+  for (column in cols2order_rows) {
+    row_order <- order(pretty_calibs[,column])
+    pretty_calibs <- pretty_calibs[row_order,]
+  }
 
   # Assign a node name
   # number_of_tips <- min(pretty_calibs$mrca_node_number)-1
   # some_table$mrca_node_name <- paste0("n", (pretty_calibs$mrca_node_number - number_of_tips))
 
   # Keep columns of interest and order them
-  pretty_calibs <- some_table[ , cols2keep_order]
+  if(missing(cols2keep_order)) {
+    cols2keep_order <- c("mrca_node_name", "taxonA", "taxonB", "MaxAge", "reference")
+  }
+  pretty_calibs <- pretty_calibs[ , cols2keep_order]
 
   # Modify column names to be informative
+  if (missing(colnames_new)) {
+    colnames_new <- c("Node Name", "taxon A", "taxon B", "Node Age", "Study reference")
+  }
   colnames(pretty_calibs) <- colnames_new
 
   # substitute underscore by space:
